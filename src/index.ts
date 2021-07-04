23112e0e910d9d0d09b9a37d5b9a26765ef47f70
Matt Noakes <mattnoakes@me.com>
2021-07-04 12:46:54 +0100

    go, swift, dart, dotnet

--- src/index.ts
+++ src/index.ts
@@ -38,39 +38,115 @@ class Spinup extends Command {
       } in directory ${flags["dir-name"] ?? args.name}`
     );
 
+    let projectDir = "";
     switch (args.type) {
       case "node":
-        // Use tmp to create a unique temporary path
-        setGracefulCleanup();
-        const tempDir = dirSync();
-
-        // Create the project directory
-        const newProjDir = join(tempDir.name, args.name);
-        mkdirSync(newProjDir);
+        projectDir = makeTempDir(args.name);
 
         // In the project directory, call `npm init`
         spawn("npm", ["init", "esm", "-y"], {
           stdio: "inherit",
-          cwd: newProjDir,
+          cwd: projectDir,
         }).on("close", (_code, _signal) =>
           // Move (and potentially rename) the directory to the working directory
           renameSync(
-            newProjDir,
+            projectDir,
             join(process.cwd(), flags["dir-name"] ?? args.name)
           )
         );
         break;
+      case "go":
+        projectDir = makeTempDir(args.name);
+
+        spawn("go", ["mod", "init", `github.com/thenoakes/${args.name}`], {
+          stdio: "inherit",
+          cwd: projectDir,
+        }).on("close", (_code, _signal) =>
+          // Move (and potentially rename) the directory to the working directory
+          renameSync(
+            projectDir,
+            join(process.cwd(), flags["dir-name"] ?? args.name)
+          )
+        );
+        break;
+      case "swift":
+        projectDir = makeTempDir(args.name);
+
+        spawn(
+          "swift",
+          ["package", "init", "--type", "executable", "--name", args.name],
+          {
+            stdio: "inherit",
+            cwd: projectDir,
+          }
+        ).on("close", (_code, _signal) =>
+          // Move (and potentially rename) the directory to the working directory
+          renameSync(
+            projectDir,
+            join(process.cwd(), flags["dir-name"] ?? args.name)
+          )
+        );
+        break;
+      case "dart":
+        projectDir = makeTempDir();
+
+        spawn("dart", ["create", "-t", "console-full", args.name], {
+          stdio: "inherit",
+          cwd: projectDir,
+        }).on("close", (_code, _signal) =>
+          // Move (and potentially rename) the directory to the working directory
+          renameSync(
+            join(projectDir, args.name),
+            join(process.cwd(), flags["dir-name"] ?? args.name)
+          )
+        );
+        break;
+      case "dotnet":
+        projectDir = makeTempDir();
+
+        spawn("dotnet", ["new", "console", "--name", args.name], {
+          stdio: "inherit",
+          cwd: projectDir,
+        }).on("close", (_code, _signal) =>
+          // Move (and potentially rename) the directory to the working directory
+          renameSync(
+            join(projectDir, args.name),
+            join(process.cwd(), flags["dir-name"] ?? args.name)
+          )
+        );
+      // case "ts":
+      //   projectDir = makeTempDir();
+
+      //   spawn("npx", ["tsdx", "create", args.name], {
+      //     stdio: "inherit",
+      //     cwd: projectDir,
+      //   }).on("close", (_code, _signal) =>
+      //     // Move (and potentially rename) the directory to the working directory
+      //     renameSync(
+      //       join(projectDir, args.name),
+      //       join(process.cwd(), flags["dir-name"] ?? args.name)
+      //     )
+      //   );
+      //   break;
       default:
         break;
     }
   }
 }
 
-// const renamePackageJson = (path: string, newName: string) => {
-//   const pJson = JSON.parse(readFileSync(join(path, "package.json"), "utf8"));
-//   pJson.name = newName;
-//   writeFileSync(join(path, "package.json"), JSON.stringify(pJson, null, 2));
+const makeTempDir = (subDir?: string) => {
+  // Use tmp to create a unique temporary path
+  setGracefulCleanup();
+  const tempDir = dirSync();
 
-// }
+  // Create the project directory
+  if (subDir) {
+    const newProjDir = join(tempDir.name, subDir);
+    mkdirSync(newProjDir);
+    return newProjDir;
+  } else {
+    return tempDir.name;
+  }
+};
 
 export = Spinup;
